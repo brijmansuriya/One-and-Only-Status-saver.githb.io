917425ffbff1201ba58e8da6ddd11899146545e7
MansuriyaBrij <brijmansuriya.ai@gmail.com>
2023-01-19 23:09:37 +0530

    add css and js

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,13 +19,38 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  // css: [],
+
+  link: [
+    { type: 'text/css', rel: 'stylesheet', href: 'vendor/bootstrap/css/bootstrap.min.css' },
+    { type: 'text/css', rel: 'stylesheet', href: 'vendor/fontawesome-free/css/all.min.css' },
+    { type: 'text/css', rel: 'stylesheet', href: 'vendor/animate/animate.compat.css' },
+    { type: 'text/css', rel: 'stylesheet', href: 'vendor/simple-line-icons/css/simple-line-icons.min.css' },
+    { type: 'text/css', rel: 'stylesheet', href: 'vendor/owl.carousel/assets/owl.carousel.min.css' },
+    { type: 'text/css', rel: 'stylesheet', href: 'vendor/owl.carousel/assets/owl.theme.default.min.css' },
+    { type: 'text/css', rel: 'stylesheet', href: 'vendor/magnific-popup/magnific-popup.min.css' },
+    { type: 'text/css', rel: 'stylesheet', href: 'css/theme.css' },
+    { type: 'text/css', rel: 'stylesheet', href: 'css/theme-elements.css' },
+    { type: 'text/css', rel: 'stylesheet', href: 'vendor/circle-flip-slideshow/css/component.css' },
+    { type: 'text/css', rel: 'stylesheet', href: 'css/skins/default.css' },
+    { type: 'text/css', rel: 'stylesheet', href: 'css/custom.css' },
+    { type: 'text/css', rel: 'stylesheet', href: 'vendor/modernizr/modernizr.min.js' },
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
+  script: [
+    { src: 'vendor/plugins/js/plugins.min.js' },
+    { src: 'js/theme.js' },
+    { src: 'vendor/circle-flip-slideshow/js/jquery.flipshow.min.js' },
+    { src: 'js/views/view.home.js' },
+    { src: 'js/theme.init.js' },
+    ],
+
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
+  
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
@@ -49,4 +74,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  publicRuntimeConfig: {
+
+  },
+  privateRuntimeConfig: {
+    
+  }
 }
